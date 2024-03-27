565021c292f36daf8a98eebeac9409e65f70b506
Gabriel Wechta <gabriel.wechta@nask.pl>
2024-03-27 15:02:19 +0100

    Fix plotter. Now it looks nice

--- plotter/window.py
+++ plotter/window.py
@@ -1,41 +1,53 @@
 import tkinter as tk
 from tkinter import ttk
 
-import matplotlib.pyplot as plt
-import numpy as np
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 
 class Window:
     def __init__(self, plotter):
+        self.plotter = plotter
         self.root = tk.Tk()
+        self.init_basic_window()
+
+    def init_basic_window(self):
         self.root.title("Slider and Plot")
-        frame = ttk.Frame(self.root, padding="10")
+        frame = ttk.Frame(self.root, padding="100")
         frame.grid(row=0, column=0)
-        slider_label = ttk.Label(frame, text="Slider")
-        slider_label.grid(row=0, column=0, sticky=tk.W)
-        self.slider = ttk.Scale(frame, from_=-10, to=10, orient=tk.HORIZONTAL, length=200, command=self.update_plot)
-        self.slider.grid(row=1, column=0)
-        fig, self.ax = plt.subplots()
-        self.ax.set_xlabel('X values')
-        self.ax.set_ylabel('Y values')
-        self.canvas = FigureCanvasTkAgg(fig, master=frame)
+        self.sliders = []
+        for i in range(4):
+            if i % 2 == 0:
+                slider_label = ttk.Label(frame, text=f"b_{i//2}")
+                slider = tk.Scale(frame, from_=-100, to=101, tickinterval=2, orient=tk.HORIZONTAL, length=300, command=lambda value, idx=i: self.update_plot(idx))
+                slider.grid(row=i//2, column=0)
+            else:
+                slider_label = ttk.Label(frame, text=f"b_{i//2}")
+                slider = tk.Scale(frame, from_=100, to=-101, tickinterval=2, orient=tk.VERTICAL, length=300, command=lambda value, idx=i: self.update_plot(idx))
+                slider.grid(row=i//2, column=1)
+            slider_label.grid(row=i//2, column=0, sticky=tk.N)
+            slider.bind("<ButtonRelease-1>", lambda event, idx=i: self.update_plot(idx))
+            self.sliders.append(slider)
+
+        self.canvas = FigureCanvasTkAgg(self.plotter.fig, master=frame)
         self.canvas.draw()
-        self.canvas.get_tk_widget().grid(row=0, column=1, rowspan=2)
-        # slider.bind("<ButtonRelease-1>", on_slider_move)
-        self.update_plot(1)
+        self.canvas.get_tk_widget().grid(row=0, column=2, rowspan=4)
+        self.update_plot(-1)
         self.root.mainloop()
 
-    def update_plot(self, value):
-        value = float(value)
-        x = np.linspace(-10, 10, 100)
-        y = np.sin(value * x)
-        self.ax.clear()
-        self.ax.plot(x, y)
+    def update_plot(self, slider_idx):
+        value = float(self.sliders[slider_idx].get())/10
+        self.plotter.refresh_ax()
+        match slider_idx:
+            case 0: self.plotter.lattice.basis.elements[0][0] = value
+            case 1: self.plotter.lattice.basis.elements[0][1] = value
+            case 2: self.plotter.lattice.basis.elements[1][0] = value
+            case 3: self.plotter.lattice.basis.elements[1][1] = value
+            case -1: pass
+
+        self.plotter.lattice.refresh_lattice()
+        self.plotter.plot_lattice()
         self.canvas.draw()
 
-    def on_slider_move(self, event):
-        self.update_plot(self.slider.get())
 
 if __name__ == '__main__':
     window = Window(None)
--- src/lattice.py
+++ src/lattice.py
@@ -1,3 +1,5 @@
+import math
+
 from plotter.plotter import Plotter
 from plotter.window import Window
 from src.matrix import Matrix
@@ -10,38 +12,37 @@ class Lattice:
         self.basis = basis
         self.spanned_elements = []
         self.span()
-        # self.minkowski_first_theorem_distance = self.basis.minkowski_first_theorem_distance()
+        self.minkowski_first_theorem_distance = self.get_minkowski_first_theorem_distance()
 
     def span(self):
-        # find range for x
+        self.spanned_elements = []
+        # find range for x and y
         x_pointer = min(self.basis.elements, key=lambda x: abs(x[0]) if abs(x[0]) != 0 else float('inf'))
         y_pointer = min(self.basis.elements, key=lambda x: abs(x[1]) if abs(x[1]) != 0 else float('inf'))
-        z_x_r = 0
-        while scalar_prod(z_x_r, x_pointer)[0] < 10:
-            z_x_r += 1
-        print(z_x_r)
-        z_x_l = 0
-        while scalar_prod(z_x_l, x_pointer)[0] > -10:
-            z_x_l -= 1
-        z_y_r = 0
-        while scalar_prod(z_y_r, y_pointer)[1] < 10:
-            z_y_r += 1
-        z_y_l = 0
-        while scalar_prod(z_y_l, y_pointer)[1] > -10:
-            z_y_l -= 1
-        z_r = max(z_x_r, z_y_r)
-        z_l = min(z_x_l, z_y_l)
-
+        x_pointer_x = abs(x_pointer[0])
+        y_pointer_y = abs(y_pointer[1])
+        z_x = int(10 / x_pointer_x)
+        z_y = int(10 / y_pointer_y)
+        z_range = max(3*max(z_x, z_y), 30)
         self.spanned_elements.extend(self.basis.elements)
-        for z1 in range(z_l, z_r + 1):
-            for z2 in range(z_l, z_r + 1):
+        for z1 in range(-z_range, z_range + 1):
+            for z2 in range(-z_range, z_range + 1):
                 v = add_vectors(scalar_prod(z1, self.basis.elements[0]), scalar_prod(z2, self.basis.elements[1]))
                 self.spanned_elements.append(v)
 
+    def refresh_lattice(self):
+        self.span()
+        self.minkowski_first_theorem_distance = self.get_minkowski_first_theorem_distance()
+
+    def get_minkowski_first_theorem_distance(self):
+        return math.sqrt(self.dimension) * self.basis.get_determinant() ** (1 / self.dimension)
+
 
 if __name__ == '__main__':
-    basis = Matrix(elements=[[1, 1], [-2, 1]])
+    basis = Matrix(elements=[[-5, 1], [2, -2]])
     lattice = Lattice(basis)
+    lattice.span()
     plotter = Plotter(lattice=lattice)
+    plotter.plot_lattice()
+    plotter.show()
     window = Window(plotter=plotter)
-    window.open()
